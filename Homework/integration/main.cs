c2a13cc4d337f8d92167083b4c9c304c0eb19f2d
laur7769 <202106153@post.au.dk>
2025-06-19 12:05:38 +0200

    finished integration C.

--- Homework/integration/main.cs
+++ Homework/integration/main.cs
@@ -1,7 +1,7 @@
 using static System.Console;
 using static System.Math;
 class main{
-    static double integrate(System.Func<double,double> f, double a, double b,
+    static (double, double) integrate(System.Func<double,double> f, double a, double b,
     double acc=0.001, double eps=0.001, double f2= double.NaN, double f3= double.NaN) // NaN indicates first call
     {
         if(double.IsPositiveInfinity(b) && double.IsNegativeInfinity(a)){
@@ -23,8 +23,13 @@ class main{
         double Q = (2*f1+f2+f3+2*f4)/6*(b-a); // higher order rule
         double q = (  f1+f2+f3+  f4)/4*(b-a); // lower order rule
         double err = Abs(Q-q);
-        if (err <= acc+eps*Abs(Q)) return Q;
-        else return integrate(f,a,(a+b)/2,acc/Sqrt(2),eps,f1,f2)+ integrate(f,(a+b)/2,b,acc/Sqrt(2),eps,f3,f4);
+            if (err <= acc + eps * Abs(Q)) return (Q, err);
+            else
+            {
+                var (Q1, err1) = integrate(f, a, (a + b) / 2, acc / Sqrt(2), eps, f1, f2);
+                var (Q2, err2) = integrate(f, (a + b) / 2, b, acc / Sqrt(2), eps, f3, f4);
+                return (Q1 + Q2, Sqrt(err1 * err1 + err2 * err2));
+            }
         }
     }
     static double erf(double z, double accuracy=0.001, double epsilon=0.001){
@@ -33,11 +38,11 @@ class main{
         }
         else if(0<=z && z<=1){
             System.Func<double,double> F = (x) => Exp(-Pow(x,2));
-            return 2.0/Sqrt(PI)*integrate(F, 0, z, acc:accuracy, eps:epsilon );
+            return 2.0/Sqrt(PI)*integrate(F, 0, z, acc:accuracy, eps:epsilon ).Item1;
         }
         else{
             System.Func<double,double> F = (t) => Exp(-Pow(z+(1-t)/t, 2))/t/t;
-            return 1-2.0/Sqrt(PI)*integrate(F, 0, 1, acc:accuracy, eps:epsilon);
+            return 1-2.0/Sqrt(PI)*integrate(F, 0, 1, acc:accuracy, eps:epsilon).Item1;
         }
     }
 
@@ -47,7 +52,7 @@ class main{
 	    return false;
     }
 
-    public static double CC_var(System.Func<double,double> f, double a, double b, double accu=0.001, double epsi=0.001){
+    public static (double, double) CC_var(System.Func<double,double> f, double a, double b, double accu=0.001, double epsi=0.001){
         System.Func<double,double> new_f = (theta) => f((a+b)/2+(b-a)/2*Cos(theta))*Sin(theta)*(b-a)/2;
         return integrate(new_f, 0.0, PI, acc:accu, eps:epsi );
     }
@@ -56,13 +61,13 @@ class main{
         WriteLine("A. Recursive open 4-point adaptive integrator");
         WriteLine("Test iimplemented integrator with certain known definite integrals:");
         System.Func<double,double> f1 = (x) => Sqrt(x);
-        WriteLine($"    * ∫_0^1 dx √(x) = 2/3. Is integrate(√(x), 0, 1)=2/3 with acc=0.001 and eps=0.001? {approx(integrate(f1, 0, 1), 2.0/3.0, acc:0.001, eps:0.001)}");
+        WriteLine($"    * ∫_0^1 dx √(x) = 2/3. Is integrate(√(x), 0, 1)=2/3 with acc=0.001 and eps=0.001? {approx(integrate(f1, 0, 1).Item1, 2.0/3.0, acc:0.001, eps:0.001)}");
         System.Func<double,double> f2 = (x) => 1.0/Sqrt(x);
-        WriteLine($"    * ∫_0^1 dx 1/√(x) = 2. Is integrate(1/√(x), 0, 1)=2 with acc=0.001 and eps=0.001? {approx(integrate(f2, 0, 1), 2.0, acc:0.001, eps:0.001)}");
+        WriteLine($"    * ∫_0^1 dx 1/√(x) = 2. Is integrate(1/√(x), 0, 1)=2 with acc=0.001 and eps=0.001? {approx(integrate(f2, 0, 1).Item1, 2.0, acc:0.001, eps:0.001)}");
         System.Func<double,double> f3 = (x) => 4.0*Sqrt(1-Pow(x,2));
-        WriteLine($"    * ∫_0^1 dx 4*√(1-x^2) = π. Is integrate(4*√(1-x^2), 0, 1)=π with acc=0.001 and eps=0.001? {approx(integrate(f3, 0, 1), PI, acc:0.001, eps:0.001)}");
+        WriteLine($"    * ∫_0^1 dx 4*√(1-x^2) = π. Is integrate(4*√(1-x^2), 0, 1)=π with acc=0.001 and eps=0.001? {approx(integrate(f3, 0, 1).Item1, PI, acc:0.001, eps:0.001)}");
         System.Func<double,double> f4 = (x) => Log(x)/Sqrt(x);
-        WriteLine($"    * ∫_0^1 dx ln(x)/√(x) = -4. Is integrate(ln(x)/√(x), 0, 1)=-4 with acc=0.001 and eps=0.001? {approx(integrate(f4, 0, 1), -4.0, acc:0.001, eps:0.001)}");
+        WriteLine($"    * ∫_0^1 dx ln(x)/√(x) = -4. Is integrate(ln(x)/√(x), 0, 1)=-4 with acc=0.001 and eps=0.001? {approx(integrate(f4, 0, 1).Item1, -4.0, acc:0.001, eps:0.001)}");
         var data1 = new System.IO.StreamWriter("data_A.txt", append:true);
         double[] erf_tab = {0.0, 0.520499878, 0.842700793, 0.966105146, 0.995322265, 0.999593048, 0.999977910, 0.999999257};
         double[] zs = {0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5};
@@ -91,22 +96,39 @@ class main{
         int ncalls = 0;
         System.Func<double,double> f_b1 = z => {ncalls++;return 1.0/Sqrt(z);};
         WriteLine("Integral to evaluate: ∫_0^1 dx 1/√(x) = 2");
-        WriteLine($"    - Computed value using implemented Clenshaw-Curtis: {CC_var(f_b1, 0.0, 1.0)}, integrand evaluations: {ncalls}");
+        WriteLine($"    - Computed value using implemented Clenshaw-Curtis: {CC_var(f_b1, 0.0, 1.0).Item1}, integrand evaluations: {ncalls}");
         ncalls=0;
-        WriteLine($"    - Computed value using only 4-point open adaptive quadrature: {integrate(f_b1, 0.0, 1.0)}, integrand evaluations: {ncalls} ");
+        var result1 = integrate(f_b1, 0.0, 1.0);
+        WriteLine($"    - Computed value using only 4-point open adaptive quadrature: {result1.Item1}, integrand evaluations: {ncalls} ");
         WriteLine($"    - Computed value using scipy's quad: 1.9999999999999993, integrand evaluations: 231");
         WriteLine("Integral to evaluate: ∫_0^1 dx ln(x)/√(x) = -4");
         ncalls=0;
         System.Func<double,double> f_b2 = z => {ncalls++;return Log(z)/Sqrt(z);};
-        WriteLine($"    - Computed value using implemented Clenshaw-Curtis: {CC_var(f_b2, 0.0, 1.0)}, integrand evaluations: {ncalls}");
+        WriteLine($"    - Computed value using implemented Clenshaw-Curtis: {CC_var(f_b2, 0.0, 1.0).Item1}, integrand evaluations: {ncalls}");
         ncalls=0;
-        WriteLine($"    - Computed value using only 4-point open adaptive quadrature: {integrate(f_b2, 0.0, 1.0)}, integrand evaluations: {ncalls} ");
+        var result2 = integrate(f_b2, 0.0, 1.0);
+        WriteLine($"    - Computed value using only 4-point open adaptive quadrature: {result2.Item1}, integrand evaluations: {ncalls} ");
         WriteLine($"    - Computed value using scipy's quad: -3.99999999999998273, integrand evaluations: 315");
         ncalls=0;
         WriteLine("Integral to evaluate: ∫_0^∞ dx exp(-x)*x^2 = 2");
         System.Func<double,double> f_b3 = z => {ncalls++;return Exp(-z)*Pow(z,2);};
-        WriteLine($"    - Computed value using implemented integrator: {integrate(f_b3, 0.0, double.PositiveInfinity)}, integrand evaluations: {ncalls}");
+        var result3 = integrate(f_b3, 0.0, double.PositiveInfinity);
+        WriteLine($"    - Computed value using implemented integrator: {result3.Item1}, integrand evaluations: {ncalls}");
         WriteLine($"    - Computed value using scipy's quad: 2.0, integrand evaluations: 165");
+        WriteLine();
+        WriteLine("C. Error estimate");
+        WriteLine(" The implemented integrator has been modified to also return the estimated error.");
+        WriteLine(" The same three integrals from exercise B. are evaluated with the same acc and eps.");
+        WriteLine("Integral to evaluate: ∫_0^1 dx 1/√(x) = 2");
+        WriteLine($"     - Estimated error: {result1.Item2}");
+        WriteLine($"     - Actual error: {Abs(result1.Item1 - 2)}");
+        WriteLine("Integral to evaluate: ∫_0^1 dx ln(x)/√(x) = -4");
+        WriteLine($"     - Estimated error: {result2.Item2}");
+        WriteLine($"     - Actual error: {Abs(result2.Item1 + 4)}");
+        WriteLine(" Integral to evaluate: ∫_0^∞ dx exp(-x)*x^2 = 2");
+        WriteLine($"     - Estimated error: {result3.Item2}");
+        WriteLine($"     - Actual error: {Abs(result3.Item1 - 2)}");
+        WriteLine(" As seen above, the estimated error is slightly higher than the actual error at this acc and eps level.");
 
         return 0;
 
