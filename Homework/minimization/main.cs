ebdaa30f4c370af42aafcf3cdb92c67e4a89889a
laur7769 <202106153@post.au.dk>
2025-06-25 11:46:52 +0200

    Touch ups on minimization homework

--- Homework/minimization/main.cs
+++ Homework/minimization/main.cs
@@ -40,7 +40,7 @@ class main{
         int counter = 0;
         do{ /* Newton's iterations */
             counter += 1;
-            if(counter>=1000) break;
+            if(counter>=2000) break;
             vector g = gradient(f, x);
             matrix H = hessian(f,x);
             if(central_ax){g = central(f,x).Item1; H = central(f,x).Item2;}
@@ -88,38 +88,53 @@ public static matrix hessian(System.Func<vector,double>f, vector x){
     return H;
 }
 
-public static (vector, matrix) central(System.Func<vector,double>f, vector x){
-    vector gf = new vector(x.size);
-    matrix H = new matrix(x.size, x.size);
-    double fx = f(x);
-    for(int i=0; i<x.size; i++){
-        double dxi = Abs(x[i])*Pow(2,-13) ;
-        x[i]+=dxi;
-        double fx_plus = f(x);
-        x[i]-=2*dxi;
-        double fx_minus = f(x);
-        x[i]+=dxi;
-        gf[i] = (fx_plus-fx_minus)/(2*dxi);
-        for(int k=0; k<x.size; k++){
-            if(k==i){H[k, i] =(fx_plus-2*fx+fx_minus)/Pow(dxi,2);}
-            else{H[i, k]=0;
-            double dxk = Abs(x[k])*Pow(2,-13);
-            x[k]+=dxk;
-            x[i] +=dxi;
-            double f1 = f(x);
-            x[i]-=2*dxi;
-            double f2 =f(x);
-            x[k]-=2*dxk;
-            double f4 = f(x);
-            x[i]+=2*dxi;
-            double f3 =f(x);
-            H[i, k] = (f1-f2-f3+f4)/(4*dxk*dxi);
-            x[i]-=dxi;
-            x[k]+=dxk;}
+    public static (vector, matrix) central(System.Func<vector, double> f, vector x)
+    {
+        vector gf = new vector(x.size);
+        matrix H = new matrix(x.size, x.size);
+        double fx = f(x);
+        for (int i = 0; i < x.size; i++)
+        {
+            double dxi = Abs(x[i]) * Pow(2, -13);
+            x[i] += dxi;
+            double fx_plus = f(x);
+            x[i] -= 2 * dxi;
+            double fx_minus = f(x);
+            x[i] += dxi;
+            gf[i] = (fx_plus - fx_minus) / (2 * dxi);
+            for (int k = 0; k < x.size; k++)
+            {
+                if (k == i) { H[k, i] = (fx_plus - 2 * fx + fx_minus) / Pow(dxi, 2); }
+                else
+                {
+                    H[i, k] = 0;
+                    double dxk = Abs(x[k]) * Pow(2, -13);
+                    x[k] += dxk;
+                    x[i] += dxi;
+                    double f1 = f(x);
+                    x[i] -= 2 * dxi;
+                    double f2 = f(x);
+                    x[k] -= 2 * dxk;
+                    double f4 = f(x);
+                    x[i] += 2 * dxi;
+                    double f3 = f(x);
+                    H[i, k] = (f1 - f2 - f3 + f4) / (4 * dxk * dxi);
+                    x[i] -= dxi;
+                    x[k] += dxk;
+                }
+            }
         }
+        return (gf, H);
     }
-    return (gf, H); 
-}
+    public static bool mini(vector sol, double accu)
+        {
+            vector min1 = new vector(-2.805118, 3.131312);
+            vector min2 = new vector(3.0, 2.0);
+            vector min3 = new vector(-3.779310, -3.283186);
+            vector min4 = new vector(3.584428, -1.848126);
+            if (sol.approx(min1, acc: accu) || sol.approx(min2, acc: accu) || sol.approx(min3, acc: accu) || sol.approx(min4, acc: accu)) return true;
+            else return false;
+        }
     public static int Main(string[] args){
         System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("en-GB");
         WriteLine("A. Newton's method with numerical gradient, numerical Hessian and back-tracking line-search");
@@ -127,24 +142,33 @@ public static (vector, matrix) central(System.Func<vector,double>f, vector x){
         WriteLine("        It is known that the Rosenbrock's valley function only has one extremum, a global minimum at (1,1)");
         System.Func<vector,double>Rosen_grad = (vec) => Pow(1-vec[0],2)+100*Pow(vec[1]-vec[0]*vec[0],2);
         vector ros_res = newton(Rosen_grad, new vector(7.0, -2.0), acc:1e-4).Item1;
-        WriteLine($"        Computed root with start (7.0, -2.0) and acc=1e-4: {(ros_res[0],ros_res[1])}");
+        WriteLine($"        Computed minimum with start (7.0, -2.0) and acc=1e-4: {(ros_res[0],ros_res[1])}");
+        WriteLine($"        Is the comuted root equal to the global minimum within the given accuracy? {ros_res.approx(new vector(1.0, 1.0), acc: 1e-4)}");
         WriteLine($"        Number of newton steps: {newton(Rosen_grad, new vector(7.0, -2.0), acc:1e-4).Item2}");
         vector ros_res2 = newton(Rosen_grad, new vector(7.0, -2.0), acc:1e-4, central_ax:true).Item1;
         WriteLine($"        Computed root with start (7.0, 2.0) and acc=1e-4, with central difference aproximation: {(ros_res2[0],ros_res2[1])}");
+        WriteLine($"        Is the computed minimum equal to the known minimum within the given accuracy? {ros_res2.approx(new vector(1.0, 1.0), acc: 1e-4)}");
         WriteLine($"        Number of newton steps: {newton(Rosen_grad, new vector(7.0, -2.0), acc:1e-4, central_ax:true).Item2}");
         WriteLine("     - Find minimum(s) ofthe Himmelblau's function: f(x,y) = (x^2+y-11)^2+(x+y^2-7)^2");
         System.Func<vector,double>Himmel_grad = (vec) => Pow(vec[0]*vec[0]+vec[1]-11,2)+Pow(vec[0]+vec[1]*vec[1]-7,2);
         WriteLine("        It is known that Himmelblau's funciton has 4 minimums: (-2.805118, 3.131312), (3.0, 2.0), (-3.779310, -3.283186) and (3.584428, -1.848126)") ;
-    var points = new List<(double, double)> {(10, 10)};
+
+    var points = new List<(double, double)> {(10.0, 10.0), (-10.0, -10.0), (-10.0, 10.0), (10.0, -10.0)};
     vector xx = new vector(2);
-        foreach ((double p1, double p2) point in points) {
+        foreach ((double p1, double p2) point in points)
+        {
             xx[0] = point.p1; xx[1] = point.p2;
-            vector solution = newton(Himmel_grad, xx, acc:1e-10).Item1;
-            vector solution2 = newton(Himmel_grad, xx, acc:1e-10, central_ax:true).Item1;
-            WriteLine($"        Computed root with start {(point.p1, point.p2)} and acc:1e-10: {(solution[0], solution[1])}");
-            WriteLine($"        Number of newton steps: {newton(Himmel_grad, xx, acc:1e-10).Item2}");
-            WriteLine($"        Computed root with start {(point.p1, point.p2)} and acc:1e-10 and central difference approximation: {(solution2[0], solution2[1])}");
-            WriteLine($"        Number of newton steps: {newton(Himmel_grad, xx, acc:1e-10, central_ax:true).Item2}");
+            vector solution = newton(Himmel_grad, xx, acc: 1e-6).Item1;
+            vector solution2 = newton(Himmel_grad, xx, acc: 1e-6, central_ax: true).Item1;
+            WriteLine($"        Computed root with start {(point.p1, point.p2)} and acc:1e-6: {(solution[0], solution[1])}");
+            WriteLine($"        Is computed minimum equal to one of the known minimums within the given accuracy? {mini(solution, 1e-6)}");
+            WriteLine($"        Number of newton steps: {newton(Himmel_grad, xx, acc: 1e-6).Item2}");
+            WriteLine();
+            WriteLine($"        Computed root with start {(point.p1, point.p2)} and acc:1e-6 and central difference approximation: {(solution2[0], solution2[1])}");
+            WriteLine($"        Is computed minimum equal to one of the known minimums within given accuracy? {mini(solution2, 1e-6)}");
+            WriteLine($"        Number of newton steps: {newton(Himmel_grad, xx, acc: 1e-6, central_ax: true).Item2}");
+            WriteLine();
+            WriteLine();
         }
     WriteLine();
     WriteLine("B. Higgs bosson discovery");
@@ -173,7 +197,7 @@ public static (vector, matrix) central(System.Func<vector,double>f, vector x){
         return sum;
         
     };
-    WriteLine("     - Computed constants for the Breit-Wigner fucntion with initial guess (126, 2, 10): ");
+    WriteLine("     - Computed constants for the Breit-Wigner fucntion with initial guess (126, 2, 10) and and accuracy of 1e-10: ");
     vector Higgs_res = newton(D, new vector(126.0, 2.0, 10.0), acc:1e-10).Item1;
     WriteLine($"         m = {Higgs_res[0]},   Gamma={Higgs_res[1]},  A={Higgs_res[2]}");
     WriteLine($"     - newton steps: {newton(D, new vector(126.0, 2.0, 10.0), acc:1e-10).Item2}");
@@ -183,7 +207,10 @@ public static (vector, matrix) central(System.Func<vector,double>f, vector x){
     }
     writer.WriteLine();
     writer.WriteLine();
-    WriteLine("The plot of the experimental data and the fit with the parameters above, can be seen in Higgs.gnuplot.svg");
+    WriteLine("     The plot of the experimental data and the fit with the parameters above, can be seen in Higgs.gnuplot.svg");
+    WriteLine($"     Even though the width should be around 4.1 MeV, which is magnitudes lower than {Higgs_res[1]} GeV, the computed Gamma is reasonable.");
+    WriteLine("     This is the case, as its experimentally very hard to obtain the pysical width, as it is so small.");
+
     WriteLine();
     WriteLine("C. Central instead of forward finite difference approximation for the derivatives");
     vector Higgs_res2 = newton(D, new vector(126.0, 2.0, 10.0), acc:1e-10, central_ax:true).Item1;
@@ -194,7 +221,8 @@ public static (vector, matrix) central(System.Func<vector,double>f, vector x){
         writer.WriteLine($"{i}  {Higgs_res2[2]/(Pow(i-Higgs_res2[0],2)+Higgs_res2[1]*Higgs_res2[1]/4.0)}");
     }
     writer.Close();
-    WriteLine("");
+    WriteLine("     It is seen how rougly the same minimum is found, but the central difference approximation uses significaltly fewer netwon steps.");
+    WriteLine("      This is however not the case when the central difference approximation is used to minimize Himmelblau's and Rosenbrock's valley functions.");
     return 0;
     }
 
